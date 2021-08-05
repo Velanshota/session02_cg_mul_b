43f5c9415101155fa45a41d8a95338de662969b4
Velandou <est.santiago.velan@unimilitar.edu.co>
2021-08-05 15:44:58 -0500

    Signed-off-by: Velandou <est.santiago.velan@unimilitar.edu.co>

---
+++ SistemCoordenadas2D/src/sistemcoordenadas2d/SistemCoordenadas2D.java
@@ -0,0 +1,24 @@
+package sistemcoordenadas2d;
+
+public class SistemCoordenadas2D {
+
+    public static void main(String[] args) {
+        Cartes2D punto =new Cartes2D();
+    }
+    
+}
+class Cartes2D{
+    private int x, y;
+    public void setX(int x){
+        this.x=x;
+    }
+    public void setY(int y){
+        this.y=y;
+    }
+   }
+class Polar{
+    private int radio;
+    private float angulo;
+    
+        
+}
